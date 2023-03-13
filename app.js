44f6018f483f5a372c7d348b29f2bb6a0f54b9ae
hiramramirez <hiram.j.ramirez@gmail.com>
2023-03-13 13:03:27 -0600

    I think everything is working propperly now

--- app.js
+++ app.js
@@ -15,22 +15,22 @@ let total = document.getElementById('total')
 asignado.innerHTML = presupuesto
 
 let sociedadDeSocorroPorcentaje = 0.10
-let quorumDeElderesPorcentaje = 0.10
-let primariaPorcentaje = 0.10
-let mujeresJovenesPorcentaje = 0.10
-let hombresJovenesPorcentaje = 0.10
+let quorumDeElderesPorcentaje = 0.05
+let primariaPorcentaje = 0.15
+let mujeresJovenesPorcentaje = 0.15
+let hombresJovenesPorcentaje = 0.15
 let jaPorcentaje = 0.10
-let escuelaDominicalPorcentaje = 0.10
-let obispadoPorcentaje = 0.30
+let escuelaDominicalPorcentaje = 0.05
+let obispadoPorcentaje = 0.25
 
-sociedadDeSocorro.innerHTML = presupuesto * sociedadDeSocorroPorcentaje
-quorumDeElderes.innerHTML = presupuesto * quorumDeElderesPorcentaje
-primaria.innerHTML = presupuesto * primariaPorcentaje
-mujeresJovenes.innerHTML = presupuesto * mujeresJovenesPorcentaje
-hombresJovenes.innerHTML = presupuesto * hombresJovenesPorcentaje
-ja.innerHTML = presupuesto * jaPorcentaje
-escuelaDominical.innerHTML = presupuesto * escuelaDominicalPorcentaje
-obispado.innerHTML = presupuesto * obispadoPorcentaje
+sociedadDeSocorro.innerHTML = (presupuesto * sociedadDeSocorroPorcentaje).toFixed(2)
+quorumDeElderes.innerHTML = (presupuesto * quorumDeElderesPorcentaje).toFixed(2)
+primaria.innerHTML = (presupuesto * primariaPorcentaje).toFixed(2)
+mujeresJovenes.innerHTML = (presupuesto * mujeresJovenesPorcentaje).toFixed(2)
+hombresJovenes.innerHTML = (presupuesto * hombresJovenesPorcentaje).toFixed(2)
+ja.innerHTML = (presupuesto * jaPorcentaje).toFixed(2)
+escuelaDominical.innerHTML = (presupuesto * escuelaDominicalPorcentaje).toFixed(2)
+obispado.innerHTML = (presupuesto * obispadoPorcentaje).toFixed(2)
 
 let totalCalculo = (sociedadDeSocorroPorcentaje * presupuesto) + (quorumDeElderesPorcentaje * presupuesto) + (primariaPorcentaje * presupuesto) + (mujeresJovenesPorcentaje * presupuesto) + (hombresJovenesPorcentaje * presupuesto) + (jaPorcentaje * presupuesto) + (escuelaDominicalPorcentaje * presupuesto) + (obispadoPorcentaje * presupuesto)
-total.innerHTML = totalCalculo
+total.innerHTML = (totalCalculo).toFixed(2)
