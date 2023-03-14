faf7d1a57a2791e4afdc44aa3fe139cf95dc9cb3
hiramramirez <hiram.j.ramirez@gmail.com>
2023-03-14 00:39:33 -0600

    expenses are working fine

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let presupuesto = 8884.80
+let presupuesto = 8885
 
 let asignado = document.getElementById('asignado')
 
@@ -23,14 +23,51 @@ let jaPorcentaje = 0.10
 let escuelaDominicalPorcentaje = 0.05
 let obispadoPorcentaje = 0.25
 
-sociedadDeSocorro.innerHTML = (presupuesto * sociedadDeSocorroPorcentaje).toFixed(2)
-quorumDeElderes.innerHTML = (presupuesto * quorumDeElderesPorcentaje).toFixed(2)
-primaria.innerHTML = (presupuesto * primariaPorcentaje).toFixed(2)
-mujeresJovenes.innerHTML = (presupuesto * mujeresJovenesPorcentaje).toFixed(2)
-hombresJovenes.innerHTML = (presupuesto * hombresJovenesPorcentaje).toFixed(2)
-ja.innerHTML = (presupuesto * jaPorcentaje).toFixed(2)
-escuelaDominical.innerHTML = (presupuesto * escuelaDominicalPorcentaje).toFixed(2)
-obispado.innerHTML = (presupuesto * obispadoPorcentaje).toFixed(2)
+let sociedadDeSocorroTotal = Math.round(presupuesto * sociedadDeSocorroPorcentaje)
+let quorumDeElderesTotal = Math.round(presupuesto * quorumDeElderesPorcentaje)
+let primariaTotal = Math.round(presupuesto * primariaPorcentaje)
+let mujeresJovenesTotal = Math.round(presupuesto * mujeresJovenesPorcentaje)
+let hombresJovenesTotal = Math.round(presupuesto * hombresJovenesPorcentaje)
+let jaTotal = Math.round(presupuesto * jaPorcentaje)
+let escuelaDominicalTotal = Math.round(presupuesto * escuelaDominicalPorcentaje)
+let obispadoTotal = Math.round(presupuesto * obispadoPorcentaje)
+
+sociedadDeSocorro.innerHTML = sociedadDeSocorroTotal
+quorumDeElderes.innerHTML = quorumDeElderesTotal
+primaria.innerHTML = primariaTotal
+mujeresJovenes.innerHTML = mujeresJovenesTotal
+hombresJovenes.innerHTML = hombresJovenesTotal
+ja.innerHTML = jaTotal
+escuelaDominical.innerHTML = escuelaDominicalTotal
+obispado.innerHTML = obispadoTotal
 
 let totalCalculo = (sociedadDeSocorroPorcentaje * presupuesto) + (quorumDeElderesPorcentaje * presupuesto) + (primariaPorcentaje * presupuesto) + (mujeresJovenesPorcentaje * presupuesto) + (hombresJovenesPorcentaje * presupuesto) + (jaPorcentaje * presupuesto) + (escuelaDominicalPorcentaje * presupuesto) + (obispadoPorcentaje * presupuesto)
-total.innerHTML = (totalCalculo).toFixed(2)
+total.innerHTML = totalCalculo
+
+let gastos = document.querySelectorAll('a')
+
+gastos.forEach((gasto) => {
+    gasto.addEventListener('click', (e) => {
+      let cantidad = prompt('¿Cuánto se gastó?')
+      let target = e.target.parentElement.parentElement.children[1].id
+      // console.log(target)
+      if (target == 'sociedad-de-socorro') {
+        sociedadDeSocorro.innerHTML -= cantidad
+      } else if (target == 'quorum-de-elderes') {
+        quorumDeElderes.innerHTML -= cantidad
+      } else if (target == 'primaria') {
+        primaria.innerHTML -= cantidad
+      } else if (target == 'mujeres-jovenes') {
+        mujeresJovenes.innerHTML -= cantidad
+      } else if (target == 'hombres-jovenes') {
+        hombresJovenes.innerHTML -= cantidad
+      } else if (target == 'ja') {
+        ja.innerHTML -= cantidad
+      } else if (target == 'escuela-dominical') {
+        escuelaDominical.innerHTML -= cantidad
+      } else if (target == 'obispado') {
+        obispado.innerHTML -= cantidad
+      }
+      total.innerHTML -= cantidad
+    })
+})
